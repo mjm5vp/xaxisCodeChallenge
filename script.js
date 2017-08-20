cfb2b11d0c7890fca4e34db51fd4294652de870e
Mark Moeller <MarkMoellerUVA@gmail.com>
2017-08-20 16:58:02 -0400

    added animation classes instead of manipulating style from js file

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-// Text
-var firstTexts = ["Insurance For", "so yours is"]
-var secondTexts = ["PEACE OF MIND", "FREE TO DREAM."]
 
 // Div variables
 var imageContainer = document.querySelector('.imageContainer')
@@ -15,28 +12,17 @@ var learnMoreButtonHover = document.querySelector('.learnMoreButtonHover')
 
 
 
-
-
-
-
-
-
-
-
-// setTimeout(function(){
-//   changeText('.firstText', firstTexts[0])
-//   changeText('.secondText', secondTexts[0])
-// },2000)
+setTimeout(function(){
+    // topText1.style.left = "0px"
+    // topText1.style.opacity = "1"
+    topText1.classList.add("anim")
+}, 1000)
 
 setTimeout(function(){
-    topText1.style.left = "0px"
-    topText1.style.opacity = "1"
-}, 3000)
-
-setTimeout(function(){
-    bottomText1.style.left = "0px"
-    bottomText1.style.opacity = "1"
-}, 3500)
+    // bottomText1.style.left = "0px"
+    // bottomText1.style.opacity = "1"
+    bottomText1.classList.add("anim")
+}, 1500)
 
 setTimeout(function(){
     topText1.style.opacity = "0"
@@ -44,7 +30,7 @@ setTimeout(function(){
 
     // firstTextDiv.style.left = "-100%"
     // secondTextDiv.style.left = "-100%"
-}, 5000)
+}, 3000)
 
 // setTimeout(function(){
 //   changeText('.firstText', firstTexts[1])
@@ -53,14 +39,16 @@ setTimeout(function(){
 
 
 setTimeout(function(){
-    topText2.style.opacity = "1"
-    topText2.style.left = "0px"
-}, 6000)
+    // topText2.style.opacity = "1"
+    // topText2.style.left = "0px"
+    topText2.classList.add("anim")
+}, 4000)
 
 setTimeout(function(){
-    bottomText2.style.opacity = "1"
-    bottomText2.style.left = "0px"
-}, 6500)
+    // bottomText2.style.opacity = "1"
+    // bottomText2.style.left = "0px"
+    bottomText2.classList.add("anim")
+}, 4500)
 
 
 
@@ -68,36 +56,46 @@ setTimeout(function(){
 
 
 setTimeout(function(){
-    legalBox.style.opacity = ".5"
+    // legalBox.style.opacity = ".5"
+    legalBox.classList.add("anim")
 
     // mouseover event
     legalBox.addEventListener('mouseover', function(){
-      legalBox.style.opacity = "0"
-      legalText.style.top = "0%"
+      // legalBox.style.opacity = "0"
+      // legalText.style.top = "0%"
+      legalText.classList.add("anim")
+      legalBox.classList.remove("anim")
+
     })
 
     // mouseout event
     legalText.addEventListener('mouseout', function(){
-      console.log("legal out")
-      legalBox.style.opacity = ".5"
-      legalText.style.top = "-10%"
+      // legalBox.style.opacity = ".5"
+      // legalText.style.top = "-10%"
+      legalText.classList.remove("anim")
+      legalBox.classList.add("anim")
     })
-}, 8000)
+}, 6000)
 
 setTimeout(function(){
-    learnMoreButton.style.opacity = "1"
-    learnMoreButton.style.right = "5%"
+    // learnMoreButton.style.opacity = "1"
+    // learnMoreButton.style.right = "5%"
+    learnMoreButton.classList.add("anim")
 
     imageContainer.addEventListener('mouseover', function(){
-      learnMoreButton.style.opacity = "0"
-      learnMoreButtonHover.style.opacity = "1"
+      // learnMoreButton.style.opacity = "0"
+      // learnMoreButtonHover.style.opacity = "1"
+      learnMoreButton.classList.add("switch")
+      learnMoreButtonHover.classList.add("switch")
     })
 
     imageContainer.addEventListener('mouseout', function(){
-      learnMoreButton.style.opacity = "1"
-      learnMoreButtonHover.style.opacity = "0"
+      // learnMoreButton.style.opacity = "1"
+      // learnMoreButtonHover.style.opacity = "0"
+      learnMoreButton.classList.remove("switch")
+      learnMoreButtonHover.classList.remove("switch")
     })
-}, 8000)
+}, 6000)
 
 
 
