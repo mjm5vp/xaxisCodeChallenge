1922f02beddb51179052012e9427d51114c0218f
Mark Moeller <MarkMoellerUVA@gmail.com>
2017-08-20 00:12:05 -0400

    legal hover working

--- script.js
+++ script.js
@@ -3,6 +3,23 @@ var firstTexts = ["Insurance For", "so yours is"]
 var secondTexts = ["PEACE OF MIND", "FREE TO DREAM."]
 
 
+
+var legalBox = document.querySelector('.legal')
+
+
+legalBox.addEventListener('mouseover', function(){
+  legalBox.innerHTML = "American Family Mutual Insurance Company, S.I., American Family Insurance Company, 6000 AmericanParkway, Madison WI 53783 | &copy; 2017 013362 - 4/17"
+  legalBox.style.padding = "5%"
+  legalBox.style.fontSize = "8px"
+})
+
+legalBox.addEventListener('mouseout', function(){
+  legalBox.innerHTML = "Legal"
+  legalBox.style.padding = "2%"
+  legalBox.style.fontSize = "15px"
+})
+
+
 setTimeout(function(){
   changeText('firstText', firstTexts[0])
   changeText('secondText', secondTexts[0])
