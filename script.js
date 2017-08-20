2c1666927206a06bb334f21377f25be71790f27d
Mark Moeller <MarkMoellerUVA@gmail.com>
2017-08-20 00:34:44 -0400

    timed fade working - restructure later

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
-
+// Text
 var firstTexts = ["Insurance For", "so yours is"]
 var secondTexts = ["PEACE OF MIND", "FREE TO DREAM."]
+var legalText = "American Family Mutual Insurance Company, S.I., American Family Insurance Company, 6000 AmericanParkway, Madison WI 53783 | &copy; 2017 013362 - 4/17"
 
-
-
+// Div variables
 var legalBox = document.querySelector('.legal')
+var firstTextDiv = document.querySelector('.firstText')
+var secondTextDiv = document.querySelector('.secondText')
 
-
+// mouseover event
 legalBox.addEventListener('mouseover', function(){
-  legalBox.innerHTML = "American Family Mutual Insurance Company, S.I., American Family Insurance Company, 6000 AmericanParkway, Madison WI 53783 | &copy; 2017 013362 - 4/17"
+  legalBox.innerHTML = legalText
   legalBox.style.padding = "5%"
   legalBox.style.fontSize = "8px"
 })
 
+// mouseout event
 legalBox.addEventListener('mouseout', function(){
   legalBox.innerHTML = "Legal"
   legalBox.style.padding = "2%"
@@ -21,20 +24,42 @@ legalBox.addEventListener('mouseout', function(){
 
 
 setTimeout(function(){
-  changeText('firstText', firstTexts[0])
-  changeText('secondText', secondTexts[0])
+  changeText('.firstText', firstTexts[0])
+  changeText('.secondText', secondTexts[0])
 },2000)
 
 setTimeout(function(){
-  changeText('firstText', firstTexts[1])
-  changeText('secondText', secondTexts[1])
-},4000)
+    firstTextDiv.style.opacity = "1"
+}, 3000)
+
+setTimeout(function(){
+    secondTextDiv.style.opacity = "1"
+}, 3500)
+
+setTimeout(function(){
+    firstTextDiv.style.opacity = "0"
+}, 5000)
+
+setTimeout(function(){
+    secondTextDiv.style.opacity = "0"
+}, 5000)
+
+setTimeout(function(){
+    firstTextDiv.style.opacity = "1"
+}, 6000)
+
+setTimeout(function(){
+    secondTextDiv.style.opacity = "1"
+}, 6500)
+
+
+setTimeout(function(){
+  changeText('.firstText', firstTexts[1])
+  changeText('.secondText', secondTexts[1])
+}, 6000)
 
 
 
-
-
-
-function changeText(id, text){
-  document.getElementById(id).innerHTML = text
+function changeText(className, text){
+  document.querySelector(className).innerHTML = text
 }
